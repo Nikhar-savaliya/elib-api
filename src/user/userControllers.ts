63551bcc5c6619707221e9254139373c16f7cefb
Nikhar S. <127284370+Nikhar-savaliya@users.noreply.github.com>
2024-04-14 11:53:52 +0530

    add login endpoint

--- src/user/userControllers.ts
+++ src/user/userControllers.ts
@@ -54,7 +54,41 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 const loginUser = async (req: Request, res: Response, next: NextFunction) => {
-  res.json({ msg: "ok" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(createHttpError(400, "all fields are required"));
+  }
+  let user: User | null;
+  try {
+    // finding user
+    user = await userModel.findOne({ email });
+    if (!user) {
+      return next(createHttpError(404, "User not found"));
+    }
+  } catch (error) {
+    return next(createHttpError(500, "error finding user from database"));
+  }
+
+  try {
+    // comparing password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return next(createHttpError(400, "email or password incorrect"));
+    }
+  } catch (error) {
+    return next(createHttpError(500, "failed to compare password"));
+  }
+  try {
+    // token generation JWT
+    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+
+    // response
+    res.status(201).json({ accessToken: token });
+  } catch (error) {
+    return next(createHttpError(500, "error while signing jwt token"));
+  }
 };
 
 export { createUser, loginUser };
--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,10 +1,10 @@
 import express from "express";
 
-import { createUser } from "./userControllers";
+import { createUser, loginUser } from "./userControllers";
 
 const userRouter = express.Router();
 
 // routes
 userRouter.post("/register", createUser);
-
+userRouter.post("/login", loginUser);
 export default userRouter;
