bdce174d2edc9f4156dfe689d8e01f3faff579b1
Nikhar S. <127284370+Nikhar-savaliya@users.noreply.github.com>
2024-04-14 12:05:35 +0530

    add book route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
 
 // global error handler
 app.use(globalErrorHandler);
