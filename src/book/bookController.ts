43629b713e753b0b349bad70b9ee30f9f3b66888
Nikhar S. <127284370+Nikhar-savaliya@users.noreply.github.com>
2024-04-15 21:23:54 +0530

    add create book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,10 +2,12 @@ import { Request, Response, NextFunction } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
+import fs from "node:fs";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   console.log(req.files);
-
+  const { title, genre } = req.body;
   try {
     const files = req.files as { [fieldname: string]: Express.Multer.File[] };
     const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
@@ -15,17 +17,20 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
       "../../public/data/uploads",
       fileName
     );
+
     const uploadResult = await cloudinary.uploader.upload(filePath, {
       filename_override: fileName,
       folder: "book-covers",
       format: coverImageMimeType,
     });
+
     const bookFilename = files.file[0].filename;
     const bookFilePath = path.resolve(
       __dirname,
       "../../public/data/uploads",
       bookFilename
     );
+
     const bookFileUploadResult = await cloudinary.uploader.upload(
       bookFilePath,
       {
@@ -35,9 +40,30 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
         format: "pdf",
       }
     );
+
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "661b72a1ff65d9c67f7618dc",
+      coverImage: uploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
+    });
+
+    // delete files from public/data/uploads
+    try {
+      await fs.promises.unlink(filePath);
+      await fs.promises.unlink(bookFilePath);
+    } catch (error) {
+      console.log(error);
+      return next(
+        createHttpError(500, "error unlinking files from public/data/uploads")
+      );
+    }
+
     return res.json({ bookFileUploadResult, uploadResult });
   } catch (error) {
-    return next(createHttpError(500, "cloudinary upload failed"));
+    console.log(error);
+    return next(createHttpError(500, "error in saving the book in database"));
   }
 };
 
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -8,7 +8,7 @@ const bookRouter = express.Router();
 
 const upload = multer({
   dest: path.resolve(__dirname, "../../public/data/uploads"),
-  limits: { fileSize: 3e7 }, //30mb
+  limits: { fileSize: 1e7 }, //10mb
 });
 
 // routes
