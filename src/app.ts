52e56d6a5ebc66e89ad501911bfe27386817830d
Nikhar S. <127284370+Nikhar-savaliya@users.noreply.github.com>
2024-04-17 19:26:17 +0530

    add cors support &  change env.sample

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
+import cors from "cors";
 
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 import bookRouter from "./book/bookRouter";
+import { config } from "./config/config";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: config.frontend_domain,
+  })
+);
 app.use(express.json());
 
 // routes
--- src/config/config.ts
+++ src/config/config.ts
@@ -12,6 +12,9 @@ const _config = {
   cloudinary_cloud: process.env.CLOUDINARY_CLOUD,
   cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
   cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
+
+  // frontend url
+  frontend_domain: process.env.FRONTEND_DOMAIN,
 };
 
 export const config = Object.freeze(_config);
